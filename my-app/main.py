bd7c75810b982776a06aa183f4d8679a1cfa826e
DarthKenar <federico_vega22@hotmail.com>
2023-10-17 20:18:07 -0300

    WIP Refresh page

--- my-app/main.py
+++ my-app/main.py
@@ -1,6 +1,32 @@
 import flet as ft
 
 word_selected="messi"
+class Enter(ft.UserControl):
+
+    def __init__(self, *args, **kwargs):
+        super(Enter, self).__init__(*args, **kwargs)
+
+    def check(self, ControlEvent):
+        print("Btn check pressed")
+        wp.panel[0].controls[2].content.value
+        self.update()
+        
+
+    def build(self):
+        
+        btn = [
+            ft.Container(
+                ft.FilledButton(
+                    text="CHECK",
+                    width=125,
+                    height=50,
+                    on_click=self.check
+                ),
+                padding=ft.padding.only(top=15,bottom=15)
+                        
+            )
+        ]
+        return ft.Row(btn,alignment=ft.MainAxisAlignment.CENTER)
 class KeyBoard(ft.UserControl):
 
     keyboard = "qwertyuiopasdfghjklñzxcvbnm"
@@ -14,15 +40,24 @@ class KeyBoard(ft.UserControl):
             ft.Row(
                 [self.key_word(j) for i, j in enumerate(self.keyboard) if i <= 9],
                 alignment="center",
-                
             ),
-            ft.Row([self.key_delete(), self.key_word("s")])
+            ft.Row(
+                [self.key_word(j) for i, j in enumerate(self.keyboard) if i > 9 and i < 20],
+                alignment="center",
+            ),
+            ft.Row([
+                ft.Container(ft.Row([self.key_word(j) for i, j in enumerate(self.keyboard) if i > 20])),
+                ft.Container(self.key_delete())],
+                alignment="center",
+            ),
         ])
     
     def key_word_action(self,ControlEvent):
-        print(f"Se presionó la letra {ControlEvent.control}")
-        # panel[fila].controls[columna].content.value = ControlEvent
-        ft.Page.update
+        print(f"Se presionó la letra {ControlEvent.control.content.value}")
+        print(wp.panel[0].controls[0].content.value)
+        wp.panel[0].controls[0].content.value = ControlEvent.control.content.value
+        self.page.update()
+        # self.page.update ""NO FUNCIONA EL UPDATE DESDE FUERA DE LA FUNCION manage?""
 
     def key_delete(self):
         key = ft.Container(
@@ -30,24 +65,26 @@ class KeyBoard(ft.UserControl):
                 icon=ft.icons.ARROW_BACK,
                 icon_size=25,
                 # on_click=self.key_delete_action
-            ),
-            padding=ft.padding.only(top=10)
+            )
         )
         return key
     def key_word(self, letter: str):
         key = ft.Container(
             ft.Text(letter,color="black"),
             on_click=self.key_word_action,
-            padding=7,
+            padding=11,
             margin=0,
             alignment=ft.alignment.center,
-            bgcolor=ft.colors.ON_SURFACE_VARIANT
+            bgcolor=ft.colors.ON_SURFACE_VARIANT,
+            border_radius=10
         )
 
         return key
+
 class WordsPanel(ft.UserControl):
     panel_row = 0
     panel_col = 0
+
     def __init__(self, *args, **kwargs,):
         super(WordsPanel, self).__init__(*args, **kwargs)
 
@@ -55,15 +92,15 @@ class WordsPanel(ft.UserControl):
         return ft.Column(self.made_panel())
     
     def made_panel(self):
-        panel = []
+        self.panel = []
         for i in range (6):
-            panel.append(self.words_display())
-        return panel
+            self.panel.append(self.words_display())
+        return self.panel
     
     def words_display(self):
-        words = []
+        self.words = []
         for i in range(5):
-            words.append(
+            self.words.append(
                 ft.Container(
                     content=ft.Text(
                         size=28,
@@ -78,12 +115,12 @@ class WordsPanel(ft.UserControl):
                 )
             )
         return ft.Row(
-            controls=words, 
+            controls=self.words, 
             alignment=ft.MainAxisAlignment.CENTER,
             )
     
+def main(page: ft.Page):
 
-def manage(page:ft.Page):
     def change_view(e):
         if page.navigation_bar.selected_index == 0:
             print("Op 1 NAVBAR")
@@ -103,13 +140,13 @@ def manage(page:ft.Page):
     page.navigation_bar = ft.NavigationBar(
         destinations=[
             ft.NavigationDestination(icon=ft.icons.FEATURED_PLAY_LIST_OUTLINED,
-                                     label="Estadísticas",
-                                     selected_icon=ft.icons.FEATURED_PLAY_LIST
-                                     ),
+                                    label="Estadísticas",
+                                    selected_icon=ft.icons.FEATURED_PLAY_LIST
+                                    ),
             ft.NavigationDestination(icon=ft.icons.PLAY_CIRCLE_OUTLINE,
-                                      label="Jugar",
-                                      selected_icon=ft.icons.PLAY_CIRCLE_FILLED_ROUNDED
-                                      ),
+                                    label="Jugar",
+                                    selected_icon=ft.icons.PLAY_CIRCLE_FILLED_ROUNDED
+                                    ),
             ft.NavigationDestination(
                 icon=ft.icons.OUTPUT,
                 label="Salir",
@@ -126,10 +163,13 @@ def manage(page:ft.Page):
     page.theme = ft.Theme(
         color_scheme_seed=ft.colors.AMBER,
     )
-    wp = WordsPanel()
-    kb = KeyBoard()
     page.add(
         wp,
-        kb
+        ent,
+        kb,
+        
     )
-ft.app(manage)
+wp = WordsPanel()
+kb = KeyBoard()
+ent = Enter()
+ft.app(main)
