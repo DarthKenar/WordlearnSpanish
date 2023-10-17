fa4da82d86c05b8fd13ca978e7f4c1453fe922c1
DarthKenar <federico_vega22@hotmail.com>
2023-10-17 06:32:47 -0300

    Refactor

--- my-app/main.py
+++ my-app/main.py
@@ -1,34 +1,80 @@
 import flet as ft
-word_selected="messi"
-keyboard = "qwertyuiopasdfghjklñzxcvbnm"
-def main(page: ft.Page):
 
-    def made_panel():
+word_selected="messi"
+class KeyBoard(ft.UserControl):
+
+    keyboard = "qwertyuiopasdfghjklñzxcvbnm"
+
+    def __init__(self, *args, **kwargs):
+        super(KeyBoard, self).__init__(*args, **kwargs)
+    
+    def build(self):
+        
+        return ft.Column([
+            ft.Row(
+                [self.key_word(j) for i, j in enumerate(self.keyboard) if i <= 9],
+                alignment="center",
+                
+            ),
+            ft.Row([self.key_delete(), self.key_word("s")])
+        ])
+    
+    def key_word_action(self,ControlEvent):
+        print(f"Se presionó la letra {ControlEvent.control}")
+        # panel[fila].controls[columna].content.value = ControlEvent
+        ft.Page.update
+
+    def key_delete(self):
+        key = ft.Container(
+            ft.IconButton(
+                icon=ft.icons.ARROW_BACK,
+                icon_size=25,
+                # on_click=self.key_delete_action
+            ),
+            padding=ft.padding.only(top=10)
+        )
+        return key
+    def key_word(self, letter: str):
+        key = ft.Container(
+            ft.Text(letter,color="black"),
+            on_click=self.key_word_action,
+            padding=7,
+            margin=0,
+            alignment=ft.alignment.center,
+            bgcolor=ft.colors.ON_SURFACE_VARIANT
+        )
+
+        return key
+class WordsPanel(ft.UserControl):
+    panel_row = 0
+    panel_col = 0
+    def __init__(self, *args, **kwargs,):
+        super(WordsPanel, self).__init__(*args, **kwargs)
+
+    def build(self):
+        return ft.Column(self.made_panel())
+    
+    def made_panel(self):
         panel = []
         for i in range (6):
-            panel.append(words_display())
+            panel.append(self.words_display())
         return panel
-
-    def top_spacing_display():
-        top_spacing = ft.Container(margin=ft.margin.only(top=50))
-        return top_spacing
-
-    def words_display():
+    
+    def words_display(self):
         words = []
         for i in range(5):
             words.append(
                 ft.Container(
                     content=ft.Text(
-                        value="X",
                         size=28,
                         width=50,
                         height=50,
-                        text_align="CENTER"
+                        text_align="CENTER",
                     ),
                     width=50,
                     height=50,
                     border_radius=ft.border_radius.all(5),
-                    bgcolor=ft.colors.SURFACE_VARIANT
+                    bgcolor=ft.colors.SURFACE_VARIANT,
                 )
             )
         return ft.Row(
@@ -36,36 +82,15 @@ def main(page: ft.Page):
             alignment=ft.MainAxisAlignment.CENTER,
             )
     
-    def check(self):
-        print("Btn check pressed")
-        panel[0].controls[2].content.value = "S"
-        page.update()
-        print(len(keyboard))
 
-    def check_display():
-
-            
-        btn = [
-            ft.Container(
-                ft.FilledButton(
-                    text="CHECK",
-                    width=125,
-                    height=50,
-                    on_click=check
-                ),
-                padding=ft.padding.only(top=50)             
-            )
-        ]
-        return ft.Row(btn,alignment=ft.MainAxisAlignment.CENTER)
-    
-    def navigation(self):
-        print("Nav change page")
+def manage(page:ft.Page):
+    def change_view(e):
         if page.navigation_bar.selected_index == 0:
-            print("Op 1 de la barra de navegacion seleccionada")
+            print("Op 1 NAVBAR")
         elif page.navigation_bar.selected_index == 1:
-            print("Op 2 de la barra de navegacion seleccionada")
+            print("Op 2 NAVBAR")
         elif page.navigation_bar.selected_index == 2:
-            print("Op 3 de la barra de navegacion seleccionada")
+            print("Op 3 NAVBAR")
             if page.platform == "windows":
                 print("Terminando aplicacion desde pc")
                 page.window_close()
@@ -74,28 +99,7 @@ def main(page: ft.Page):
                 page.window_destroy()
         else:
             pass
-
-    def key(letter):
-        key = ft.Container(
-            ft.TextButton(
-                text=letter,
-            ),
-            expand=True,
-            padding=ft.padding.only(top=10)
-        )
-        return key
-
-    
-    page.title = "Wordle Game by DK Games"
-
-    top_spacing = top_spacing_display()
-    panel = made_panel()
-    btn_check = check_display()
-
-    page.theme = ft.Theme(
-        color_scheme_seed=ft.colors.AMBER,
-    )
-
+        
     page.navigation_bar = ft.NavigationBar(
         destinations=[
             ft.NavigationDestination(icon=ft.icons.FEATURED_PLAY_LIST_OUTLINED,
@@ -112,15 +116,20 @@ def main(page: ft.Page):
                 
             ),
         ],
-        on_change=navigation
+        on_change=change_view
     )
+    page.window_width = 360
+    page.window_max_width = 360
+    page.window_height = 800
+    page.window_max_height = 800
+    page.padding = ft.padding.only(top=50)
+    page.theme = ft.Theme(
+        color_scheme_seed=ft.colors.AMBER,
+    )
+    wp = WordsPanel()
+    kb = KeyBoard()
     page.add(
-        top_spacing,
-        ft.Column(panel),
-        ft.Row(controls=[key("q"),key("w"),key("e"),key("r"),key("t"),key("y"),key("u"),key("i"),key("o"),key("p")]),
-        ft.Row(controls=[key("a"),key("s"),key("d"),key("f"),key("g"),key("h"),key("j"),key("k"),key("l")]),
-        ft.Row(controls=[key("z"),key("x"),key("c"),key("v"),key("b"),key("n"),key("m")]),
-        btn_check
+        wp,
+        kb
     )
-
-ft.app(main)
+ft.app(manage)
