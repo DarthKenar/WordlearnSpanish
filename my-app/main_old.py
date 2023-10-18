24ebb33dc3648b1a3e6184b873af3a672f96a36b
DarthKenar <federico_vega22@hotmail.com>
2023-10-17 22:31:36 -0300

    👌 IMPROVE: Now, page is refresh correclty

--- my-app/main_old.py
+++ my-app/main_old.py
@@ -1,64 +1,41 @@
 import flet as ft
 
 word_selected="messi"
-
-class WordApp(ft.UserControl):
-
-    keyboard = "qwertyuiopasdfghjklñzxcvbnm"
-    panel_row = 0
-    panel_col = 0
+class Enter(ft.UserControl):
 
     def __init__(self, *args, **kwargs):
-        super(WordApp, self).__init__(*args, **kwargs)
+        super(Enter, self).__init__(*args, **kwargs)
 
-    def build(self):
-        
-        return(ft.Column(controls=[
-            self.panel_build(),
-            self.enter_build(),
-            self.keyboard_build(),
-        ]))
-
-    def enter_action(self, ControlEvent):
+    def check(self, ControlEvent):
         print("Btn check pressed")
         self.panel[0].controls[2].content.value
         self.update()
+        
 
-    def enter_build(self):
+    def build(self):
+        
         btn = [
             ft.Container(
                 ft.FilledButton(
                     text="CHECK",
                     width=125,
                     height=50,
-                    on_click=self.enter_action
+                    on_click=self.check
                 ),
                 padding=ft.padding.only(top=15,bottom=15)
                         
             )
         ]
         return ft.Row(btn,alignment=ft.MainAxisAlignment.CENTER)
-    
-    def key_word_action(self,ControlEvent):
-        print(f"Se presionó la letra {ControlEvent.control.content.value}")
-        print(self.panel[0].controls[0].content.value)
-        self.panel[0].controls[0].content.value = ControlEvent.control.content.value
-        self.page.update()
-        
-    def key_word(self, letter: str):
-        key = ft.Container(
-            ft.Text(letter,color="black"),
-            on_click=self.key_word_action,
-            padding=11,
-            margin=0,
-            alignment=ft.alignment.center,
-            bgcolor=ft.colors.ON_SURFACE_VARIANT,
-            border_radius=10
-        )
-        return key
-    
-    def keyboard_build(self):
+class KeyBoard(ft.UserControl):
 
+    keyboard = "qwertyuiopasdfghjklñzxcvbnm"
+
+    def __init__(self, *args, **kwargs):
+        super(KeyBoard, self).__init__(*args, **kwargs)
+    
+    def build(self):
+        
         return ft.Column([
             ft.Row(
                 [self.key_word(j) for i, j in enumerate(self.keyboard) if i <= 9],
@@ -75,24 +52,50 @@ class WordApp(ft.UserControl):
             ),
         ])
     
-    def key_delete_action(self):
-        pass
-    
+    def key_word_action(self,ControlEvent):
+        print(f"Se presionó la letra {ControlEvent.control.content.value}")
+        print(wp.panel[0].controls[0].content.value)
+        wp.panel[0].controls[0].content.value = ControlEvent.control.content.value
+        self.page.update()
+        # self.page.update ""NO FUNCIONA EL UPDATE DESDE FUERA DE LA FUNCION manage?""
+
     def key_delete(self):
-        self.key = ft.Container(
+        key = ft.Container(
             ft.IconButton(
                 icon=ft.icons.ARROW_BACK,
                 icon_size=25,
                 # on_click=self.key_delete_action
             )
         )
-        return self.key
+        return key
+    def key_word(self, letter: str):
+        key = ft.Container(
+            ft.Text(letter,color="black"),
+            on_click=self.key_word_action,
+            padding=11,
+            margin=0,
+            alignment=ft.alignment.center,
+            bgcolor=ft.colors.ON_SURFACE_VARIANT,
+            border_radius=10
+        )
+
+        return key
+
+class WordsPanel(ft.UserControl):
+    panel_row = 0
+    panel_col = 0
+
+    def __init__(self, *args, **kwargs,):
+        super(WordsPanel, self).__init__(*args, **kwargs)
+
+    def build(self):
+        return ft.Column(self.made_panel())
     
-    def panel_build(self):
+    def made_panel(self):
         self.panel = []
         for i in range (6):
             self.panel.append(self.words_display())
-        return ft.Column(self.panel)
+        return self.panel
     
     def words_display(self):
         self.words = []
@@ -160,9 +163,14 @@ def main(page: ft.Page):
     page.theme = ft.Theme(
         color_scheme_seed=ft.colors.AMBER,
     )
-    wordapp = WordApp()
     page.add(
-        wordapp
+        wp,
+        ent,
+        kb,
+        
     )
     
+wp = WordsPanel()
+kb = KeyBoard()
+ent = Enter()
 ft.app(main)
